ebcb0f9cac412039ac9bcee896f17c5e1c93dddc
Robin Schellius <r.schellius@avans.nl>
2018-11-30 14:44:17 +0100

    Updated

--- les2/gameserver/server.js
+++ les2/gameserver/server.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-<<<<<<< HEAD
-const gameController = require('./src/controllers/game.controller')
-=======
 const gameRoutes = require('./src/routes/game.routes')
->>>>>>> development
 
 var app = express()
 
@@ -14,13 +10,6 @@ app.use(bodyParser.json())
 
 const port = 3000
 
-<<<<<<< HEAD
-app.get('/games', gameController.getAll)
-app.get('/games/:gameId', gameController.getById)
-app.post('/games', gameController.addNewGame)
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-=======
 app.use('/api', gameRoutes)
 
 app.use('*', (err, req, res, next) => {
@@ -32,4 +21,3 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // for testing purpose
 module.exports = app
->>>>>>> development
