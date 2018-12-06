ab33971ba1062bc68359b1361b9780d3b2870135
Robin Schellius <r.schellius@avans.nl>
2018-12-06 15:39:54 +0100

    Updated

--- les4/gameserver/src/routes/game.routes.js
+++ les4/gameserver/src/routes/game.routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const gameController = require('../controllers/game.controller')
 const authController = require('../controllers/auth.controller')
+const userController = require('../controllers/user.controller')
 
 router.get('/games', authController.validateJWT, gameController.getAll)
 router.get('/games/:gameId', authController.validateJWT, gameController.getById)
 router.post('/games', authController.validateJWT, gameController.addNewGame)
 
-router.post('/register', authController.register)
+router.post('/register', userController.validateUser, authController.register)
 router.post('/login', authController.login)
 
 
