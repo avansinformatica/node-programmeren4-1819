a5149462e3b91b459ea63919e102aff360c1e8ee
Robin Schellius <r.schellius@avans.nl>
2018-12-06 11:27:09 +0100

    Updated

--- les4/gameserver/src/controllers/auth.controller.js
+++ les4/gameserver/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
-
 const ApiError = require('../models/apierror.model')
 const pool = require('../config/db')
+const config = require('../config/config')
+const jwt = require('jsonwebtoken');
+
 
 module.exports = {
 
@@ -24,14 +26,59 @@ module.exports = {
 	login(req, res, next) {
 		console.log('AuthController.register login')
 
-		// For pool initialization, see above
-		pool.query("SELECT * FROM games", function (err, rows, fields) {
-			// Connection is automatically released when query resolves
+		const email = req.body.email || ''
+		const password = req.body.password || ''
+		console.log(`email = ${email} password = ${password}`)
+
+		pool.query("SELECT * FROM `users` WHERE `email` = ?", [req.body.email], (err, rows, fields) => {
 			if (err) {
 				console.log(err)
 				return next(new ApiError(err, 500))
 			}
-			res.status(200).json({ result: rows }).end()
+			// debug
+			console.log('user found!')
+			console.dir(rows)
+			if(rows.length === 1 && password === rows[0].password){
+				console.log('password matches')
+
+				jwt.sign({ id: rows[0].ID }, config.secretKey, (err, token) => {
+					if (err) {
+						console.log(err)
+						return next(new ApiError('Error creating token!', 500))
+					}
+					console.log(token);
+					res.status(200).json({ 
+						result: {
+							firstname: rows[0].firstname,
+							lastname: rows[0].lastname,
+							token: token
+						} 
+					}).end()
+				});
+			} else {
+				return next(new ApiError('error ', 500))
+			}
+		})
+	},
+
+	validateJWT(req, res, next) {
+
+		const token = req.header('x-access-token')
+		if(!token){
+			return next(new ApiError('Required token is missing', 401))
+		}
+
+		jwt.verify(token, config.secretKey, (err, payload) => {
+			if(err){
+				return next(new ApiError('Invalid token supplied.', 401))
+			}
+			console.log('Token successfully validated!')
+			console.dir(payload)
+			// save user and next
+			req.user = {
+				id: payload.id
+			}
+			next()
 		})
 	}
 
--- les4/gameserver/src/routes/game.routes.js
+++ les4/gameserver/src/routes/game.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const gameController = require('../controllers/game.controller')
 const authController = require('../controllers/auth.controller')
 
-router.get('/games', gameController.getAll)
+router.get('/games', authController.validateJWT, gameController.getAll)
 router.get('/games/:gameId', gameController.getById)
 router.post('/games', gameController.addNewGame)
 
