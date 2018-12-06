4ec1d66c25166fc632f0d9afc3c8e180b44610ff
Robin Schellius <r.schellius@avans.nl>
2018-12-06 15:09:34 +0100

    Udpated na les 4

---
+++ les4/gameserver/README.md
@@ -0,0 +1,16 @@
+# Programmeren 4 Gameserver - met JWT authenticatie en tests
+De gameserver met JWT token authenticatie. De voorbeeldcode in de tests gebruikt ook JWT.
+
+## Usage
+Run
+
+```
+npm install
+npm start
+```
+
+For testing:
+```
+npm test
+```
+
--- les4/gameserver/src/controllers/auth.controller.js
+++ les4/gameserver/src/controllers/auth.controller.js
@@ -9,25 +9,44 @@ module.exports = {
 		console.log('AuthController.register called')
 
 		const query = 'INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`) VALUES (?, ? ,?, ?)'
-
-		pool.query(query, 
+		pool.query(
+			query, 
 			[req.body.firstname, req.body.lastname, req.body.email, req.body.password], 
 			function (err, rows, fields) {
-			// Connection is automatically released when query resolves
-			if(err){
-				console.log(err.sqlMessage)
-				return next(new ApiError(err.sqlMessage, 500))
-			}
-			res.status(200).json({ result: rows }).end()
-		})
+				if(err){
+					console.log(err.sqlMessage)
+					return next(new ApiError(err.sqlMessage, 500))
+				}
+				// als we hier zijn was er geen error. Check de rows om te zien of alles goed is gegaan.
+				if (rows && rows.affectedRows === 1 && rows.warningStatus === 0) {
+					// console.log('User was successfully created.')
+					// Create a token and return it.
+					jwt.sign({ id: rows.insertId }, config.secretKey, (err, token) => {
+						if (err) {
+							console.log(err)
+							return next(new ApiError('Error creating token!', 500))
+						}
+						// console.log(token);
+						res.status(200).json({
+							result: {
+								firstname: req.body.firstname,
+								lastname: req.body.lastname,
+								token: token
+							}
+						}).end()
+					});
+				} else {
+					return next(new ApiError(rows, 500))
+				}
+			})
 	},
 
 	login(req, res, next) {
-		console.log('AuthController.register login')
+		console.log('AuthController.login called')
 
 		const email = req.body.email || ''
 		const password = req.body.password || ''
-		console.log(`email = ${email} password = ${password}`)
+		// console.log(`email = ${email} password = ${password}`)
 
 		pool.query("SELECT * FROM `users` WHERE `email` = ?", [req.body.email], (err, rows, fields) => {
 			if (err) {
@@ -35,8 +54,8 @@ module.exports = {
 				return next(new ApiError(err, 500))
 			}
 			// debug
-			console.log('user found!')
-			console.dir(rows)
+			// console.log('user found!')
+			// console.dir(rows)
 			if(rows.length === 1 && password === rows[0].password){
 				console.log('password matches')
 
@@ -45,7 +64,7 @@ module.exports = {
 						console.log(err)
 						return next(new ApiError('Error creating token!', 500))
 					}
-					console.log(token);
+					// console.log(token);
 					res.status(200).json({ 
 						result: {
 							firstname: rows[0].firstname,
